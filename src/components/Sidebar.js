a3043b14cf691c5d1f2e6079d4f35d514afac7a0
Ikram <ikramzulfugar@gmail.com>
2022-06-30 21:58:13 +0400

    ADD: Header

---
+++ src/components/Sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './styles/sidebar.css'
+
+function Sidebar() {
+  return (
+    <div className='sidebar' >
+        <div className='sidebar_header'>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar
